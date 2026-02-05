1b853ae0ead70e623ac8d47a55af7d27cebca614
Adham Hamdy <adhamhn333@gmail.com>
2026-02-05 22:28:23 +0200

    implement non-blocking read/write/accept/process handlers and delete blocking handlers

--- server.cpp
+++ server.cpp
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include <vector>
 
-size_t const MAX_MSG_LENGTH = 4098; // 4098 bytes
+size_t const MAX_MSG_LENGTH = (32 << 20);
 
 /*
 struct sockaddr_in {
@@ -42,8 +42,8 @@ struct connection_state {
     bool want_write = false;
     bool want_close = false;
 
-    std::vector<int32_t> incoming; // incoming data for the application logic to process
-    std::vector<int32_t> outgoing; // outgoing data from the application logic
+    std::vector<int8_t> incoming; // incoming data for the application logic to process
+    std::vector<int8_t> outgoing; // outgoing data from the application logic to send
 };
 
 void alert(char const* msg)
@@ -58,114 +58,155 @@ void log_error(char const* msg)
     abort();
 }
 
-void sokcet_set_nb(int socket)
+void buffer_append(std::vector<int8_t>& buffer, int8_t const* data, size_t len)
 {
-    fcntl(socket, F_SETFL, fcntl(socket, F_GETFL, 0) | O_NONBLOCK);
+    // append to back
+    buffer.insert(buffer.end(), data, data + len);
 }
 
-int32_t read_n(int tcp_connection, char* rbuffer, size_t n)
+void buffer_consume(std::vector<int8_t>& buffer, size_t len)
 {
-    int32_t total_read = 0;
-
-    while (total_read < n) {
-        ssize_t ret_val = read(tcp_connection, rbuffer + total_read, n - total_read);
-
-        if (ret_val > 0) {
-            total_read += ret_val;
-        } else if (ret_val == 0) { // EOF
-            return total_read;
-        } else { // error
-            if (errno == EINTR)
-                continue; // retry
-            return -1;
-        }
-    }
-
-    return total_read;
+    // remove from front
+    buffer.erase(buffer.begin() + len);
 }
 
-int32_t write_n(int tcp_connection, char const* wbuffer, size_t n)
+void socket_set_nb(int socket)
 {
-    int32_t total_write = 0;
+    int flags = fcntl(socket, F_GETFL, 0);
 
-    while (total_write < n) {
-        ssize_t ret_val = write(tcp_connection, wbuffer + total_write, n - total_write);
-        if (ret_val > 0) { // error or EOF
-            total_write += ret_val;
-        } else if (ret_val == 0) {
-            return total_write;
-        } else {
-            if (errno == EINTR)
-                continue;
-            return -1;
-        }
-    }
-
-    return total_write;
+    // O_NONBLOCK flag applies to: read() - write() - accept() - connect()
+    flags |= O_NONBLOCK;
+    fcntl(socket, F_SETFL, flags);
 }
 
-int32_t process_one_req(int tcp_connection)
+bool try_one_request(connection_state* conn)
 {
-    // 4 bytes message length + up to 4096 bytes payload
-    char rbuffer[4 + MAX_MSG_LENGTH] = {};
-
-    errno = 0;
-    // read first 4 bytes, message length
-    ssize_t ret_val = read_n(tcp_connection, rbuffer, 4);
-    if (ret_val != 4) {
-        alert(errno == 0 ? "EOF" : "read(): error");
-        return -1;
+    // message size
+    if (conn->incoming.size() < 4) {
+        return false;
     }
 
-    // read message length, first 4 bytes
-    uint32_t len = 0;
-    memcpy(&len, rbuffer, 4);
+    u_int32_t len = 0;
+    memcpy(&len, conn->incoming.data(), 4);
     if (len > MAX_MSG_LENGTH) {
-        alert("payload is too long");
-        return -1;
+        alert("message too long");
+        conn->want_close = true;
+        return false;
     }
 
-    // read the request payload
-    ret_val = read_n(tcp_connection, rbuffer + 4, len);
-    if (ret_val != len) {
-        alert(errno == 0 ? "EOF" : "read(): error");
-        return -1;
+    // message body
+    if (4 + len > conn->incoming.size()) {
+        return false;
     }
+
+    int8_t* request = &conn->incoming[4];
 
     // TODO: process the request
-    printf("client says: %.*s\n", len, &rbuffer[4]);
+    printf("client says: len:%d data:%.*s\n",
+        len, len < 100 ? len : 100, request);
 
-    char const reply[] = "world";
-    char wbuffer[4 + sizeof(reply)];
+    // send the response to outgoing buffer
+    buffer_append(conn->outgoing, (int8_t const*)&len, 4);
+    buffer_append(conn->outgoing, request, len);
 
-    len = (uint32_t)strlen(reply);
-    memcpy(wbuffer, &len, 4); // first 4 bytes of wbuffer contains the msg len
-    memcpy(&wbuffer[4], reply, len);
+    // remove the message from the incoming buffer
+    buffer_consume(conn->incoming, 4 + len);
 
-    ret_val = write_n(tcp_connection, wbuffer, 4 + len);
-    if (ret_val < 0) {
-        log_error("write()");
-    }
-
-    return 0;
+    return true;
 }
 
 connection_state* handle_accept(int tcp_socket)
 {
-    // TODO:
-    return nullptr;
+    struct sockaddr_in client_socket = {};
+    socklen_t client_address = sizeof(client_socket);
+
+    int connection_socket = accept(tcp_socket, (struct sockaddr*)&client_socket, &client_address);
+    if (connection_socket < 1) {
+        return nullptr;
+    }
+
+    uint32_t ip = client_socket.sin_addr.s_addr;
+    fprintf(stderr, "new client from %u.%u.%u.%u:%u\n",
+        ip & 255, (ip >> 8) & 255, (ip >> 16) & 255, ip >> 24,
+        ntohs(client_socket.sin_port));
+
+    // set this connection socket to non-blocking
+    socket_set_nb(connection_socket);
+
+    // create connection state for this socket
+    connection_state* conn = new connection_state();
+    conn->tcp_socekt = connection_socket;
+    conn->want_read = true; // read the first request
+
+    return conn;
 }
 
-int handle_read(connection_state* conn)
+// app logic when socket is writable
+void handle_write(connection_state* conn)
 {
-    // TODO:
-    return 0;
+    assert(conn->outgoing.size() > 0);
+
+    int ret_val = write(conn->tcp_socekt, conn->outgoing.data(), conn->outgoing.size());
+    if (ret_val < 0 && errno == EAGAIN) {
+        return; // not ready
+    }
+
+    if (ret_val < 0) {
+        alert("write() error");
+        conn->want_close = true;
+        return;
+    }
+
+    // remove written data from the outgoing buffer
+    buffer_consume(conn->outgoing, ret_val);
+
+    // state transition
+    if (conn->outgoing.size() == 0) {
+        conn->want_read = true;
+        conn->want_write = false;
+    }
 }
 
-int handle_write(connection_state* conn)
+// app logic when socket is readable
+void handle_read(connection_state* conn)
 {
-    // TODO:
-    return 0;
+    int8_t buffer[64 * 1024];
+    ssize_t ret_val = read(conn->tcp_socekt, buffer, sizeof(buffer));
+    if (ret_val < 0 && errno == EAGAIN) {
+        return; // not ready
+    }
+
+    if (ret_val < 0) {
+        alert("read() error");
+        conn->want_close = true;
+        return;
+    }
+
+    // handle EOF
+    if (ret_val == 0) {
+        if (conn->incoming.size() == 0) {
+            alert("client closed");
+        } else {
+            alert("unexpected EOF");
+        }
+        conn->want_close = true;
+        return;
+    }
+
+    // accumulate the new data to connection state incoming data buffer
+    buffer_append(conn->incoming, buffer, ret_val);
+
+    // try to parse the accumulated data
+    while (try_one_request(conn)) {
+        /* do nothing */
+    }
+
+    // state transition
+    if (conn->outgoing.size() > 0) {
+        conn->want_write = true;
+        conn->want_read = false;
+        return handle_write(conn);
+    }
 }
 
 int main()
@@ -193,7 +234,7 @@ int main()
     }
 
     // set server socket to non-blocking mode
-    sokcet_set_nb(server_socket);
+    socket_set_nb(server_socket);
 
     // listen
     result_value = listen(server_socket, SOMAXCONN); // SOMAXCONN: size of the queue
@@ -218,6 +259,10 @@ int main()
         sockets_list.push_back(socket);
 
         for (connection_state* conn : conn_state_map) {
+            if (!conn) {
+                continue;
+            }
+
             struct pollfd socket = { conn->tcp_socekt, POLLERR, 0 }; // always wake up if error happened
 
             if (conn->want_read) {
@@ -231,11 +276,11 @@ int main()
         }
 
         /// blocking - check all sockets receive/send buffers or TCP errors
-        int ret_val = poll(sockets_list.data(), sockets_list.size(), -1);
+        int ret_val = poll(sockets_list.data(), (nfds_t)sockets_list.size(), -1);
         if (ret_val < -1 && errno == EINTR) { // nothing is ready rn
             continue;
         }
-        if (ret_val < -1) {
+        if (ret_val < 0) {
             log_error("poll()");
         }
 
@@ -244,10 +289,11 @@ int main()
 
             connection_state* conn = handle_accept(listening_socket);
             if (conn) {
-                if (conn_state_map.size() <= (size_t)listening_socket) {
-                    conn_state_map.resize(listening_socket + 1);
+                if (conn_state_map.size() <= (size_t)conn->tcp_socekt) {
+                    conn_state_map.resize(conn->tcp_socekt + 1, nullptr);
                 }
-                conn_state_map[listening_socket] = conn;
+                assert(!conn_state_map[conn->tcp_socekt]);
+                conn_state_map[conn->tcp_socekt] = conn;
             }
         }
 
@@ -256,15 +302,21 @@ int main()
             connection_state* conn = conn_state_map[sockets_list[i].fd];
             uint32_t revents = sockets_list[i].revents;
 
+            if (revents == 0) {
+                continue;
+            }
+
             if (revents & POLLIN) {
+                assert(conn->want_read);
                 handle_read(conn);
             }
 
             if (revents & POLLOUT) {
+                assert(conn->want_write);
                 handle_write(conn);
             }
 
-            if (revents & POLLERR || conn->want_close) {
+            if ((revents & POLLERR) || conn->want_close) {
                 close(conn->tcp_socekt);
                 conn_state_map[conn->tcp_socekt] = nullptr;
                 delete conn;
